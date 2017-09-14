a44a9e91c10909cf22f8bd2fad08786a963ad213
flyhero <qfwang666@163.com>
2017-09-14 11:12:04 +0800

    增加基础工具类

---
+++ common/src/main/java/com/dfocus/common/constant/JSONResult.java
@@ -0,0 +1,42 @@
+package com.dfocus.common.constant;
+
+/**
+ * qfwang
+ */
+public class JSONResult {
+    protected int code;
+    protected String msg;
+    protected Object data;
+
+    public JSONResult(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static JSONResult ok(TipEnum tipEnum,Object data){
+        return new JSONResult(tipEnum.getCode(),tipEnum.getMsg(),data);
+    }
+    public static JSONResult ok(Object data){
+        return  ok(TipEnum.ok,data);
+    }
+    public static JSONResult ok(TipEnum tipEnum){
+        return  ok(tipEnum,null);
+    }
+    public static JSONResult ok(){
+        return ok(null);
+    }
+
+    public static JSONResult error(TipEnum tipEnum,Object data){
+        return new JSONResult(tipEnum.getCode(),tipEnum.getMsg(),data);
+    }
+    public static JSONResult error(Object data){
+        return  ok(TipEnum.ok,data);
+    }
+    public static JSONResult error(TipEnum tipEnum){
+        return  ok(tipEnum,null);
+    }
+    public static JSONResult error(){
+        return ok(null);
+    }
+}
---
+++ common/src/main/java/com/dfocus/common/constant/TipEnum.java
@@ -0,0 +1,46 @@
+package com.dfocus.common.constant;
+
+/**
+ * Description:
+ * User: qfwang
+ * Date: 2017-09-14
+ * Time: 上午10:46
+ */
+public enum TipEnum {
+
+    ok(200,"ok"),
+    error(500,"error");
+
+    protected int code;
+    protected String msg;
+
+    TipEnum(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public static String getMsg(int code)
+    {
+        for (TipEnum tipEnum : TipEnum.values()) {
+            if (tipEnum.getCode()== code) {
+                return tipEnum.msg;
+            }
+        }
+        return null;
+    }    
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
