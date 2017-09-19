5bc15eecaa82e55c26c89f2313984adbb6cf83bf
flyhero <qfwang666@163.com>
2017-09-19 10:04:53 +0800

    delete dashborad service

--- config-client-service/src/test/java/com/dfocus/config/client/ConfigClientServiceApplicationTests.java
+++ config-client-service/src/test/java/com/dfocus/config/client/ConfigClientServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dfoucs.dashboard;
+package com.dfocus.config.client;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DashboardServiceApplicationTests {
+public class ConfigClientServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
