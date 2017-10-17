84b6fe351fbd8bfa662ffd9dd968fba6c78b7511
flyhero <qfwang666@163.com>
2017-10-17 09:47:00 +0800

    增加docker

--- gateway-service/src/main/java/com/dfocus/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/dfocus/gateway/GatewayServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient //注册服务
+//@EnableDiscoveryClient //注册服务
 @EnableZuulProxy	//开启路由
 public class GatewayServiceApplication {
 	private static final Logger logger = LoggerFactory.getLogger(GatewayServiceApplication.class);
@@ -18,4 +18,26 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 		//logger.info("======================网关服务启动完毕=======================");
 	}
+/*	mysql001:
+	image: mysql
+	volumes:
+			- /docker/mysql/data:/var/lib/mysql
+	environment:
+	TZ: 'Asia/Shanghai'
+	MYSQL_ROOT_PASSWORD: 123456
+	ports:
+			- "3306:3306"
+	networks:
+			- "netName1"
+
+			FROM frolvlad/alpine-oraclejdk8:slim
+VOLUME /tmp
+ADD ./target/gateway-service-0.0.1-SNAPSHOT.jar app.jar
+RUN sh -c 'touch /app.jar'
+ENV JAVA_OPTS=""
+ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar" ]
+EXPOSE 8082
+
+			*/
+
 }
